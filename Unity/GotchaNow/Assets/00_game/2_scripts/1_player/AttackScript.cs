5ac18c844ca9d63680c730fa65f1f2a3a519ab1c
DukeOfOool <156922682+DukeOfOool@users.noreply.github.com>
2025-11-03 10:17:34 +0100

    Squashed commit of the following:
    commit 6b5bba2af6da3b4aaf3256daab6e855ae2c6a86e
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Sun Nov 2 16:17:18 2025 +0100
    
        late commit: animations + hitbox on sword
    
    commit 8d899e83513ae89d426b70e7217f0bebe7df2fab
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Wed Oct 22 13:55:08 2025 +0200
    
        fixed aspect ratio
    
    commit 685338b5d515716387886402862250f2f657d375
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 21:49:42 2025 +0200
    
        implemented charge attack
    
    commit 5d90b8a1f01cc90207d327be2589081612b314f8
    Author: StarlightMisaki <muriel.ackermann@zhdk.ch>
    Date:   Tue Oct 21 21:11:01 2025 +0200
    
        fixed charge attack
    
    commit 83a239d612e7fc3ca5c05e7f1ae38779dbb6ecf3
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 21:10:31 2025 +0200
    
        animation upgrade !! w root motion !! (except heavy attack)
    
    commit 5b934fadc65db29c3dec6ea77b14dab2e0975fc9
    Author: StarlightMisaki <muriel.ackermann@zhdk.ch>
    Date:   Tue Oct 21 19:00:33 2025 +0200
    
        more animations correction
    
    commit 68470e77f958c32aa4f07c5f0fb49dcbc2f9a714
    Author: StarlightMisaki <muriel.ackermann@zhdk.ch>
    Date:   Tue Oct 21 18:59:19 2025 +0200
    
        rootmotion animation
    
    commit caf417b72251197d8e8aa44dd6ee24334e986a3f
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 17:23:47 2025 +0200
    
        anim update 2 (animation in intermission, strafe walk, root motion still glitched)
    
    commit 53a71f34526fc32f42315773fb14822b9b1ea3bb
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 15:55:13 2025 +0200
    
        anim update (root motion still glitched)
    
    commit 4dcc94227e3f651eb709338daff92fef2cc6dac5
    Merge: 7456172 5725808
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 14:05:10 2025 +0200
    
        Merge branch 'mauro2' of https://github.com/DukeOfOool/Ongoing_GotchaNow into mauro2
    
    commit 74561727cf27437c0517f05b75053c37c8303e96
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 14:04:53 2025 +0200
    
        quick commit
    
    commit 572580857c0d2d2b73d6a1822830f5c3d67cae65
    Author: StarlightMisaki <muriel.ackermann@zhdk.ch>
    Date:   Tue Oct 21 14:03:23 2025 +0200
    
        new animations and model
    
    commit 24651ba35d653de3c368ddb322419b663b789395
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Tue Oct 21 13:55:37 2025 +0200
    
        implementing player character model & animation
    
    commit d1bdd5c9935fa5d7df38cc4eb805f45967e2cfb4
    Author: StarlightMisaki <muriel.ackermann@zhdk.ch>
    Date:   Mon Oct 20 16:03:23 2025 +0200
    
        added animations for player
    
    commit 24c05593ac8a246ec8332e09584b1937472a2b58
    Author: virtuademi <146850367+virtuademi@users.noreply.github.com>
    Date:   Mon Oct 20 15:22:06 2025 +0200
    
        fixed merge issues & a lock-on bug

--- Unity/GotchaNow/Assets/00_game/2_scripts/1_player/AttackScript.cs
+++ Unity/GotchaNow/Assets/00_game/2_scripts/1_player/AttackScript.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 public class AttackScript : MonoBehaviour
 {
     public GameObject hitbox;
+    public GameObject hitbox2;
+    public string attackType;
+    public float windupTime;
+    public float windup2Time;
+    public float endTime;
+    public float hitboxDmg;
+    public float hitbox2Dmg;
     public string attackType;
     public float windupTime;
 
@@ -13,16 +20,85 @@ public class AttackScript : MonoBehaviour
         {
             StartCoroutine(PlayerAttack());
         }
+        /* else if (attackType == "playerHeavyAttack")
+        {
+            StartCoroutine(PlayerHeavyAttack());
+        } */
         else
         {
-            hitbox.SetActive(true);
+        if (attackType == "playerAttack")
+        {
+            StartCoroutine(PlayerAttack());
+        }
+        else
+        {
+                hitbox.SetActive(true);
+        }
         }
     }
+
+    
 
     public void EndAttack()
     {
         hitbox.SetActive(false);
     }
+
+    private IEnumerator PlayerAttack()
+    {
+        float timer = 0;
+
+        hitbox.GetComponent<AttackBox>().damage = hitboxDmg;
+
+        while (timer < windupTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        hitbox.SetActive(true);
+
+        while (timer < endTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        hitbox.SetActive(false);
+    }
+
+    /* private IEnumerator PlayerHeavyAttack()
+    {
+        float timer = 0;
+
+        hitbox.GetComponent<AttackBox>().damage = hitboxDmg;
+        hitbox2.GetComponent<AttackBox>().damage = hitbox2Dmg;
+
+        while (timer < windupTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        hitbox.SetActive(true);
+
+        while (timer < windup2Time)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        hitbox2.SetActive(true);
+
+        while (timer < endTime)
+        {
+            timer += Time.deltaTime;
+            yield return null;
+        }
+
+        hitbox.SetActive(false);
+        hitbox2.SetActive(false);
+    } */
 
     private IEnumerator PlayerAttack()
     {
