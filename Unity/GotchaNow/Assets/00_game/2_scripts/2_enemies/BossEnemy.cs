b7090df989cde2a2b0a46c59f99b04e911b9d081
virtuademi <146850367+virtuademi@users.noreply.github.com>
2025-11-09 15:47:41 +0100

    walls around arena (WIP), started working on enemy behavior

--- Unity/GotchaNow/Assets/00_game/2_scripts/2_enemies/BossEnemy.cs
+++ Unity/GotchaNow/Assets/00_game/2_scripts/2_enemies/BossEnemy.cs
@@ -1,4 +1,6 @@
 using System.Collections;
+using System.Threading;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class BossEnemy : MonoBehaviour
@@ -9,6 +11,9 @@ public class BossEnemy : MonoBehaviour
     public GameObject model;
     private bool actionInProgress = false;
     private float distance;
+    public float walkSpeed = 3;
+    public float strafeSpeed = 2;
+    public float turnSpeed = 0.3f;
     public GameObject attack1;
     public float attack1Duration;
     public float attack1EndingLag;
@@ -24,12 +29,17 @@ public class BossEnemy : MonoBehaviour
 
     void OnEnable()
     {
-        pb = PlayerBattle.Instance;   
+        pb = PlayerBattle.Instance;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        /* //test
+        LookAtPlayer(1);
+        WalkTowardsPlayer(1);
+        StrafeAroundPlayer("r", 1); */
+        
         if (!actionInProgress)
         {
             ChooseAction();
@@ -44,27 +54,87 @@ public class BossEnemy : MonoBehaviour
     private void ChooseAction()
     {
         distance = enemy.DistanceCheck(pb.gameObject.transform.position);
-        //Debug.Log("distance to player: " + distance);
+        Debug.Log("distance to player: " + distance);
 
-        StartCoroutine(Attack1());
+        if (distance <= 3)
+        {
+            StartCoroutine(Attack1());
+        }
+        else
+        {
+            //approach player
+
+            StartCoroutine(ApproachPlayer(1));
+        }
+
         actionInProgress = true;
     }
 
-    private void WalkTowardsPlayer()
+    private void LookAtPlayer(float speedMult)
     {
+        Vector3 pVector = pb.gameObject.transform.position - rb.position;
+        pVector.y = 0;
 
+        transform.LookAt(Vector3.Lerp(rb.position + transform.forward, rb.position + pVector.normalized, turnSpeed * speedMult));
     }
 
-    private void StrafeAroundPlayer()
+    private void WalkTowardsPlayer(float speedMult)
     {
+        Vector3 pVector = pb.gameObject.transform.position - rb.position;
+        pVector.y = 0;
 
+        rb.MovePosition(rb.position + pVector.normalized * walkSpeed * speedMult * Time.fixedDeltaTime);
     }
     
-    private void AvoidPlayer()
+    private void AvoidPlayer(float speedMult)
     {
-        
+        Vector3 pVector = pb.gameObject.transform.position - rb.position;
+        pVector.y = 0;
+
+        rb.MovePosition(rb.position - pVector.normalized * walkSpeed * speedMult * Time.fixedDeltaTime);
     }
 
+    private void StrafeAroundPlayer(string dir, float speedMult)
+    {
+        /* Vector3 pVector = pb.gameObject.transform.position - transform.position;
+        pVector.y = 0; */
+
+        if (dir == "r")
+        {
+            rb.MovePosition(rb.position + transform.right * strafeSpeed * speedMult * Time.fixedDeltaTime);
+        }
+
+        if (dir == "l")
+        {
+            rb.MovePosition(rb.position - transform.right * strafeSpeed * speedMult * Time.fixedDeltaTime);
+        }
+    }
+
+    private IEnumerator ApproachPlayer(float actionTime)
+    {
+        float timer = 0;
+
+        //WIP: set strafe direction
+        int rndm = Random.Range(0, 2);
+        string strafeDir;
+        if (rndm == 0) strafeDir = "l";
+        else strafeDir = "r";
+
+        while (timer < actionTime)
+        {
+            timer += Time.fixedDeltaTime;
+
+            LookAtPlayer(1);
+            WalkTowardsPlayer(1);
+            StrafeAroundPlayer(strafeDir, 1);
+
+            yield return new WaitForFixedUpdate();
+        }
+
+        actionInProgress = false;
+    }
+
+    //test attack
     private IEnumerator Attack1()
     {
         AttackScript atkScript = attack1.GetComponent<AttackScript>();
@@ -94,6 +164,7 @@ public class BossEnemy : MonoBehaviour
         actionInProgress = false;
     }
 
+    //when attack is parried by player
     private IEnumerator AttackParried(float stunTime, float knockback)
     {
         float timer = 0;
