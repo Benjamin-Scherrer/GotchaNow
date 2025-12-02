db9100b84a93e901384c45dbb0720e8e2def2c4b
virtuademi <146850367+virtuademi@users.noreply.github.com>
2025-12-02 14:54:00 +0100

    öpis vergesse bim meteor

--- Unity/GotchaNow/Assets/00_game/2_scripts/1_player/Meteor.cs
+++ Unity/GotchaNow/Assets/00_game/2_scripts/1_player/Meteor.cs
@@ -18,6 +18,8 @@ public class Meteor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {        
+        ProgressionManager.instance.EndBattleEvent.AddListener(EndBattle);
+        
         lockOnTarget = LockOnTarget.instance;
         targetEnemy = BattleManager.instance.activeEnemy[0];
 
